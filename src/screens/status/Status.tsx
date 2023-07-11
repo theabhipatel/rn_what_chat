5c076bf15a084b9a7018c327fb7b06c301d67afd
TheAbhiPatel <abhishek.patel@stackinfinite.com>
2023-07-11 16:40:07 +0530

    Add : upload status button added

--- src/screens/status/Status.tsx
+++ src/screens/status/Status.tsx
@@ -1,4 +1,11 @@
-import {Image, StyleSheet, Text, View, FlatList} from 'react-native';
+import {
+  Image,
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  TouchableOpacity,
+} from 'react-native';
 import React, {useCallback, useEffect, useState} from 'react';
 import StatusHeader from './components/StatusHeader';
 import StatusNotSeen from './components/StatusNotSeen';
@@ -10,15 +17,20 @@ import {ImagePickerResponse} from 'react-native-image-picker';
 import uploadFile from '../../utils/uploadFile';
 import OpenCameraOrGalleryModal from './components/OpenCameraOrGalleryModal';
 
+export interface IUserInfo {
+  email: string;
+  photo: string;
+  userId: string;
+}
+
 const Status = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [imageData, setImageData] = useState<ImagePickerResponse>({});
-  const [userInfo, setUserInfo] = useState({
+  const [userInfo, setUserInfo] = useState<IUserInfo>({
     email: '',
     photo: '',
     userId: '',
   });
-  console.log('------ imageData --------->>', imageData);
 
   useEffect(() => {
     getUserInfo();
@@ -45,6 +57,7 @@ const Status = () => {
             <StatusHeader
               setIsModalOpen={setIsModalOpen}
               photo={userInfo.photo}
+              userId={userInfo.userId}
             />
           )}
           ItemSeparatorComponent={() => <View style={{marginVertical: 8}} />}
@@ -68,6 +81,22 @@ const Status = () => {
             </View>
           )}
         />
+        <TouchableOpacity onPress={() => setIsModalOpen(true)}>
+          <View
+            style={{
+              position: 'absolute',
+              bottom: 16,
+              right: 10,
+              padding: 10,
+              backgroundColor: '#128C7E',
+              borderRadius: 20,
+            }}>
+            <Image
+              source={require('../../images/camera.png')}
+              style={{width: 20, height: 20, tintColor: '#fff'}}
+            />
+          </View>
+        </TouchableOpacity>
       </View>
       <OpenCameraOrGalleryModal
         isModalOpen={isModalOpen}
